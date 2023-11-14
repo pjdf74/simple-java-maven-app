d38e54fa524a294cf80d52a1813e2f8dbc500e6e
pjdf74 <150827104+pjdf74@users.noreply.github.com>
2023-11-14 20:08:30 +0000

    Fix to clean variables from special characters

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -24,8 +24,18 @@ VERSION=$(mvn help:evaluate -Dexpression=project.version | grep "^[^\[]")
 VERSION=$(echo "$VERSION" | tr -d '\r\n')
 set +x
 
+clean_name=$(echo "$NAME" | sed 's/\x1B\[[0-9;]*[JKmsu]//g')
+clean_version=$(echo "$VERSION" | sed 's/\x1B\[[0-9;]*[JKmsu]//g')
+
+echo "Cleaned NAME: $clean_name"
+echo "Cleaned VERSION: $clean_version"
+
+# Construct the file path using cleaned variables
+FILE="target/${clean_name}-${clean_version}.jar"
+
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
 
-java -jar "target/${NAME}-${VERSION}.jar"
+#java -jar "target/${NAME}-${VERSION}.jar"
+java -jar ${FILE}
